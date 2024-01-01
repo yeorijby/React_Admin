3927beb2c5cce6c8ac00165e8d0698a8566bdff2
Kalitsma <yeorijby@gmail.com>
2024-01-01 22:40:25 +0900

    이벤트 쪽이 이상해서 처음부터 다시 하고 있는데 이벤트 추가시 특정 일자에만 이벤르로 계속 추가됨

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,34 +1,34 @@
 import { useState, useEffect, useRef } from "react";
 import {
   Box,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  useTheme,
   Dialog,
   DialogTitle,
   DialogContent,
   TextField,
   Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
   Select,
-  MenuItem,
+  MenuItem,  
 } from "@mui/material";
-import Header from "../../components/Header";
 import { formatDate } from "@fullcalendar/core";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import Header from "../../components/Header";
 import { tokens } from "../../theme";
 
 const Calendar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [currentEvents, setCurrentEvents] = useState([]);
-  const [isDialogOpen, setDialogOpen] = useState(false);
-  const [formData, setFormData] = useState({
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [eventData, setEventData] = useState({
     title: "",
     classType: "",
     memberName: "",
@@ -57,131 +57,86 @@ const Calendar = () => {
   }, []);
 
   const handleDialogClose = () => {
-    //alert("handleDialogClose");
     setDialogOpen(false);
+  };
+  // const handleDateClick = (selected) => {
+  //   setEventData({
+  //     title: "",
+  //     classType: "",
+  //     memberName: "",
+  //     attendance: "",
+  //   });
+  //   setDialogOpen(true);
+  // };
+// handleDateClick 함수 수정
+const handleDateClick = (info) => {
+  setEventData({
+    title: "",
+    classType: "",
+    memberName: "",
+    attendance: "",
+    start: info.date,
+    end: info.date,
+  });
+
+  setDialogOpen(true);
+};
+
+const handleConfirmClick = () => {
+  const { title, classType, memberName, attendance } = eventData;
+  const calendarApi = calendarRef.current?.getApi();
+
+  if (calendarApi) {
+    const currentDate = calendarApi.view.activeStart;
+
+    // 예제 코드에서는 하나의 선택된 날짜만을 고려하고 있습니다.
+    // 만약 다중 선택이 가능하다면 해당 부분을 수정해야 합니다.
+    const selectedDate = currentDate;
+
+    // end를 하루 뒤의 날짜로 설정
+    const endDate = new Date(selectedDate);
+    endDate.setDate(selectedDate.getDate() + 1);
+
+    calendarApi.addEvent({
+      id: `${selectedDate.toISOString()}-${title}`,
+      title,
+      start: selectedDate,
+      end: endDate,
+      allDay: true,
+      classType,
+      memberName,
+      attendance,
+    });
+  }
+
+  handleDialogClose();
+};
+
+
+
+
+  
+  
+
+
+  const handleInputChange = (fieldName) => (event) => {
+    setEventData({ ...eventData, [fieldName]: event.target.value });
   };
 
   const handleEventClick = (selected) => {
-    if (window.confirm(`이 일정을 진짜로 지우고 싶은겨? '${selected.event.title}'`)) {
+    if (
+      window.confirm(
+        `이 일정을 진짜로 지우고 싶은겨? '${selected.event.title}'`
+      )
+    ) {
       selected.event.remove();
-
-      setCurrentEvents((prevEvents) => {
-        const updatedEvents = prevEvents.filter((event) => event.id !== selected.event.id);
-        return updatedEvents;
-      });
     }
-  };
-
-  // 옮길때 관여하는 넘인듯
-  const handleEventDrop = (dropInfo) => {
-    const updatedEvents = currentEvents.map((event) =>
-      event.id === dropInfo.event.id
-        ? {
-            
-            ...event,
-            start: dropInfo.event.start,
-            end: dropInfo.event.end,
-          }
-        : event
-    );
-
-    setCurrentEvents(updatedEvents);
-  };
-
-  const handleEventContent = (arg) => {
-    const classType = arg.event.extendedProps.classType;
-    const backgroundColor = getClassTypeColor(classType);
-    // return {
-    //   ...arg.view,
-    //   backgroundColor,
-    // };
-    //alert("handleEventContent");
-
-    return {
-        html: `<div style="background-color: ${backgroundColor};">${arg.event.title}</div>`,
-    };
-  };
-
-  const getClassTypeColor = (classType) => {
-    //alert("10");
-    switch (classType) {
-      case "A":
-        return colors.primary[500];
-      case "B":
-        return colors.greenAccent[500];
-      case "C":
-        return colors.blueAccent[500];
-      default:
-        return colors.grey[500];
-    }
-  };
-
-  const handleDateClick = (info) => {
-    setDialogOpen(true);
-    setCurrentEvents([...currentEvents, info]);
-    setFormData({
-      title: "",
-      classType: "",
-      memberName: "",
-      attendance: "",
-    });
-  };
-
-  const handleFormSubmit = () => {
-    const { title, classType, memberName, attendance } = formData;
-
-    const calendarApi = calendarRef.current.getApi();
-
-    if (title) {
-      calendarApi.addEvent({
-        id: `${currentEvents[0].dateStr}-${title}`,
-        title,
-        start: currentEvents[0].startStr,
-        end: currentEvents[0].endStr,
-        allDay: currentEvents[0].allDay,
-        classType,
-        memberName,
-        attendance,
-      });
-      setDialogOpen(false);
-
-      // 기존 currentEvents를 업데이트하지 않고 새로운 이벤트만 추가
-      setCurrentEvents((prevEvents) => [
-        ...prevEvents,
-        {
-          id: `${currentEvents[0].dateStr}-${title}`,
-          title,
-          start: currentEvents[0].startStr,
-          end: currentEvents[0].endStr,
-          allDay: currentEvents[0].allDay,
-          classType,
-          memberName,
-          attendance,
-        },
-      ]);
-    }
-  };  
-
-  // FullCalendar의 dayRender 이벤트를 사용하여 토요일은 파란색, 일요일 및 공휴일은 빨간색으로 스타일링 - 안댐
-  const handleDayRender = (info) => {
-    const dayOfWeek = info.date.getDay(); // 0은 일요일, 1은 월요일, ..., 6은 토요일
-
-    if (dayOfWeek === 0 || dayOfWeek === 6) {
-      // 일요일 또는 토요일인 경우
-      info.el.style.color = dayOfWeek === 0 ? colors.redAccent[900] : colors.blueAccent[900];
-      //info.el.style.color = 'white';
-    }
-  };
-
-  const handleInputChange = (field) => (event) => {
-    setFormData({ ...formData, [field]: event.target.value });
   };
 
   return (
     <Box m="20px">
       <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
       <Box display="flex" justifyContent="space-between">
-        {/* CALENDAR BAR */}
         <Box
           flex="1 1 20%"
           backgroundColor={colors.primary[400]}
@@ -194,7 +149,7 @@ const Calendar = () => {
               <ListItem
                 key={event.id}
                 sx={{
-                  backgroundColor: getClassTypeColor(event.classType), //colors.greenAccent[500],
+                  backgroundColor: colors.greenAccent[500],
                   margin: "10px 0",
                   borderRadius: "2px",
                 }}
@@ -202,30 +157,19 @@ const Calendar = () => {
                 <ListItemText
                   primary={event.title}
                   secondary={
-                    <Box
-                        color={colors.grey[500]}
-                    >
-                      <Typography>
-                        {formatDate(event.start, {
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                        })}
-                      </Typography>
-                      <br />
-                      Class Type: {event.classType}
-                      <br />
-                      Member Name: {event.memberName}
-                      <br />
-                      Attendance: {event.attendance}
-                    </Box>
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
                   }
                 />
               </ListItem>
             ))}
           </List>
         </Box>
-        {/* CALENDAR */}
         <Box flex="1 1 100%" ml="15px">
           <FullCalendar
             ref={calendarRef}
@@ -241,53 +185,29 @@ const Calendar = () => {
             selectable={true}
             selectMirror={true}
             dayMaxEvents={true}
-            weekends={true}
-            dayRender={handleDayRender} // 추가된 부분
-            select={(info) => {
-              setCurrentEvents([info]);         // 
-              handleDateClick(info);
-            }}
-            eventTextColor={colors.grey[500]}
+            select={handleDateClick}
             eventClick={handleEventClick}
-            eventDrop={handleEventDrop} 
-            eventContent={handleEventContent} 
+            eventsSet={(events) => setCurrentEvents(events)}
             initialEvents={[
-              {
-                id: "1234",
-                title: "All-day event",
-                date: "2023-12-14",
-                classType: "A",
-                memberName: "홍길동",
-                attendance: "출석",
-              },
-              {
-                id: "4321",
-                title: "Timed event",
-                date: "2023-12-28",
-                classType: "B",
-                memberName: "홍길서",
-                attendance: "결석",
-              },
-              ...currentEvents, // 추가된 부분
+              { id: "1234", title: "All-day event", date: "2024-01-14" },
+              { id: "4321", title: "Timed event", date: "2024-01-28" },
             ]}
           />
         </Box>
       </Box>
-
-      {/* Custom Form Dialog */}
-      <Dialog open={isDialogOpen} onClose={handleDialogClose} backgroundColor={colors.primary[400]}>
-        <DialogTitle color={colors.grey[100]}>Add Event</DialogTitle>
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>이벤트 정보 입력</DialogTitle>
         <DialogContent>
           <TextField
             label="Title"
-            value={formData.title}
+            value={eventData.title}
             onChange={handleInputChange("title")}
             fullWidth
             margin="normal"
           />
           <Select
             label="Class Type"
-            value={formData.classType}
+            value={eventData.classType}
             onChange={handleInputChange("classType")}
             fullWidth
             margin="normal"
@@ -298,23 +218,19 @@ const Calendar = () => {
           </Select>
           <TextField
             label="Member Name"
-            value={formData.memberName}
+            value={eventData.memberName}
             onChange={handleInputChange("memberName")}
             fullWidth
             margin="normal"
           />
           <TextField
             label="Attendance"
-            value={formData.attendance}
+            value={eventData.attendance}
             onChange={handleInputChange("attendance")}
             fullWidth
             margin="normal"
           />
-          <Box display="flex" justifyContent="end" mt="20px">
-            <Button onClick={handleFormSubmit} color="secondary" variant="contained">
-              이벤트를 추가할겨
-            </Button>
-          </Box>
+          <Button onClick={handleConfirmClick}>확인</Button>
         </DialogContent>
       </Dialog>
     </Box>
